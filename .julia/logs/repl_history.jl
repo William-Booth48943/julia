d1df14382f708add7f071bfbc858bc149f52dc18
William Booth <boothy.w@gmail.com>
2021-03-03 10:11:30 +0000

    add csv

---
+++ .julia/logs/repl_history.jl
@@ -0,0 +1,6 @@
+# time: 2021-03-03 09:26:27 GMT Standard Time
+# mode: julia
+	import Pkg; Pkg.add("Csv");
+# time: 2021-03-03 10:03:38 GMT Standard Time
+# mode: julia
+	import Pkg; Pkg.add("CSV");
